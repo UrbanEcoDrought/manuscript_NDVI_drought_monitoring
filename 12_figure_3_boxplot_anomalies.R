e6d791269687997cf9fca5a980b3cddc732fcc6f
jharr101 <JHarr@mortonarb.org>
2025-08-28 09:30:46 -0500

    minor plot fixes

--- 12_figure_3_boxplot_anomalies.R
+++ 12_figure_3_boxplot_anomalies.R
@@ -25,7 +25,7 @@ p1 <- ggplot()+ #boxplots by drought category for each LC type
   geom_text(data=grow_sum, aes(label=tukey_USDM_category, x=severity,y=mean_anom-sd),size = 3, vjust=2, hjust =-1)+
   facet_wrap(~type)+
   geom_hline(yintercept=0, linetype="dashed")+
-  labs(caption = "lowercase = effect of drought severity on NDVI anomalies within a given land cover class \nuppercase = effect of land cover type on NDVI anomalies within a given drought category") +
+  #labs(caption = "lowercase = effect of drought severity on NDVI anomalies within a given land cover class \nuppercase = effect of land cover type on NDVI anomalies within a given drought category") +
   ylim(-0.2,0.2) + theme_bw(15) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),plot.caption.position="plot",
                                         plot.caption = element_text(hjust=0,vjust=0.5),plot.margin = margin(5.5,5.5,20,5.5,"pt"), legend.position = "bottom")
 
@@ -40,9 +40,9 @@ grow_merge_unique <- grow_merge %>% distinct(severity, date, .keep_all = TRUE)
 #   scale_fill_manual(name="Category", values=c("None"="gray50", "D0"="yellow", "D1"="burlywood","D2"="darkorange", "D3"="red"))
 
 p2 <- ggplot(data=grow_merge_unique[grow_merge_unique$severity!="None",], aes(x=as.factor(severity), fill=as.factor(severity)))+
-  geom_bar() + xlab("USDM Category") +
+  geom_bar() + xlab("USDM Category") + ylim(0,125)+
   scale_fill_manual(name="USDM Category", values=c("D0"="#ffff00", "D1"="#fbd380","D2"="#ffaa03", "D3"="#e60001"))+
-  theme(legend.position = "none")
+  theme_bw(15) + theme(legend.position = "none")
 
 # joint figure ------------------------------------------------------------
 
@@ -57,4 +57,7 @@ ggpubr::ggarrange(
   heights = c(2,1)
 )
 
-plot_grid(p1,p2, ncol=1, rel_heights =c(2,1), labels = "auto")
+ggsave("figure_3_NDVI_anoms_boxplot.png", path = pathShare, height=8, width=12, units="in", dpi = 320)
+
+
+#plot_grid(p1,p2, ncol=1, rel_heights =c(2,1), labels = "auto")
